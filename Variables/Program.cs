c38ef780919281a1f0ac94f51d14b91d40ad13e5
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-05-29 15:33:43 -0500

    Agregar archivos de proyecto.

---
+++ Variables/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+// variables
+int number = 5;
+double PI = 3.16;
+char zone = 'A';
+string word = "Hello, world";
+bool isActive = false;
+
+static int Add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+static int Subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
+static int Multiply(int num1, int num2)
+{
+    return num1 * num2;
+}
+static double Divide(int num1, int num2)
+{
+    double result = 0.0;
+
+    if (num2 == 0)
+    {
+        Console.WriteLine("Error: It is not posible divide by 0");
+    } else
+    {
+        result = num1 / num2;
+    }
+
+    return result;
+}
+
+Console.WriteLine(Add(5, 10));
+Console.WriteLine(Subtract(5, 10));
+Console.WriteLine(Multiply(5, 10));
+Console.WriteLine(Divide(5, 10));
+
